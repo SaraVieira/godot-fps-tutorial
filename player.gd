92643976fadd8af280eaa879631b7cd5a619cfad
Sara Vieira <hey@iamsaravieira.com>
2023-03-12 17:14:45 +0100

    cant move

--- player.gd
+++ player.gd
@@ -8,7 +8,7 @@ const SPEED = 6.0
 const JUMP_VELOCITY = 6.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = 9.8
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
@@ -31,7 +31,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("left", "right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		if Input.is_action_pressed("shift") : 
--- world.gd
+++ world.gd
@@ -1,6 +1,30 @@
 extends Node
+@onready var main_menu = $"Lights & Effects/CanvasLayer/main menu"
+@onready var address_entry = $"Lights & Effects/CanvasLayer/main menu/MarginContainer/VBoxContainer/Adddress entry"
+
+var Player = preload("res://player.tscn");
+
+const PORT = 9999;
+var enet_peer = ENetMultiplayerPeer.new()
 
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().quit()	
+		get_tree().quit()
+		
+
+func _on_host_button_pressed():
+	main_menu.hide();
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	
+	add_player(multiplayer.get_unique_id())
+
+
+func _on_join_button_pressed():
+	pass # Replace with function body.
+
+func add_player(peer_id):
+	var p = Player.instantiate()
+	p.name = str(peer_id)
+	add_child(p)
